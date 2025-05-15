b21bce5153b484f076904f991584cebbcbbafd91
Abdullah Bagar <abdullah.saeed.bagar@gmail.com>
2025-05-15 03:39:55 +0300

    create theme folder

---
+++ lib/theme/app_shadows.dart
@@ -0,0 +1,10 @@
+class AppShadows {
+  const AppShadows._();
+
+  // static BoxShadow shadow1 = BoxShadow(
+  //   offset: const Offset(0, 1),
+  //   blurRadius: 2,
+  //   spreadRadius: 0,
+  //   color: AppColors.shadow1Color.withOpacity(.5),
+  // );
+}
