4aa952419f40850e73a55b7e7ab8305785d92e84
Abdullah Bagar <abdullah.saeed.bagar@gmail.com>
2025-05-16 17:51:39 +0300

    Done Setting Ui

---
+++ lib/core/helper_functions/get_types_result.dart
@@ -0,0 +1,1 @@
+List<String> getTypesResult() => ["أكثر إبداع", "وقت أقصر", "وقت أطول"];
--- lib/features/settings/widgets/settings_view_body.dart
+++ lib/features/settings/widgets/settings_view_body.dart
@@ -4,6 +4,8 @@ import 'package:fahimak_ai/core/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../../core/helper_functions/get_majors_names.dart';
+import '../../../core/helper_functions/get_types_result.dart';
 import '../../../core/utils/app_colors.dart';
 
 class SettingsViewBody extends StatelessWidget {
@@ -11,103 +13,128 @@ class SettingsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Divider(height: 1, color: AppColors.primary50),
-          SizedBox(height: 20),
-          SvgPicture.asset(AppImages.aiRobotSmall, height: 150),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              color: AppColors.primary15,
-              borderRadius: BorderRadius.circular(16),
-            ),
-
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-              child: Row(
-                textDirection: TextDirection.rtl,
-                children: [
-                  SvgPicture.asset(AppImages.generate),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Text(
-                      "أهلا, لو تبغاني أعطيك نتائج أكثر دقة تناسب ماتحتاجة, قم بتخصيص الإعدادات التالية لتناسب ماتريد...",
-                      textDirection: TextDirection.rtl,
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ),
-                ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Divider(height: 1, color: AppColors.primary50),
+            SizedBox(height: 20),
+            SvgPicture.asset(AppImages.aiRobotSmall, height: 150),
+            DecoratedBox(
+              decoration: BoxDecoration(
+                color: AppColors.primary15,
+                borderRadius: BorderRadius.circular(16),
               ),
-            ),
-          ),
-          SizedBox(height: 12),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-            ),
 
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-              child: Column(
-                children: [
-                  Row(
-                    textDirection: TextDirection.rtl,
-                    children: [
-                      SvgPicture.asset(AppImages.generate),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Text(
-                          "يمكنني أن أقترح عليك بعض التخصصات التي يمكنك أن تسألني عنها...",
-                          textDirection: TextDirection.rtl,
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 12),
-                  Wrap(
-                    textDirection: TextDirection.rtl,
-                    spacing: 8,
-                    runSpacing: 8,
-                    children: List.generate(
-                      majors().length,
-                      (index) => ChipMajorWidget(
-                        nameMajor: majors()[index],
-                        isSelected: index == 0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 12,
+                ),
+                child: Row(
+                  textDirection: TextDirection.rtl,
+                  children: [
+                    SvgPicture.asset(AppImages.generate),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Text(
+                        "أهلا, لو تبغاني أعطيك نتائج أكثر دقة تناسب ماتحتاجة, قم بتخصيص الإعدادات التالية لتناسب ماتريد...",
+                        textDirection: TextDirection.rtl,
+                        style: Theme.of(context).textTheme.bodySmall,
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 12),
+            CustomizeSettingsWidget(
+              title: "كيف تحب أن تكون إجابتي؟",
+              children: List.generate(
+                getTypesResult().length,
+                (index) => ChipWidget(
+                  name: getTypesResult()[index],
+                  isSelected: index == 0,
+                ),
+              ),
+            ),
+            SizedBox(height: 12),
+            CustomizeSettingsWidget(
+              title:
+                  "يمكنني أن أقترح عليك بعض التخصصات التي يمكنك أن تسألني عنها...",
+              children: List.generate(
+                getMajorsNames().length,
+                (index) => ChipWidget(
+                  name: getMajorsNames()[index],
+                  isSelected: index == 0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-
-  List<String> majors() => [
-    "التخصص الاول",
-    "التخصص الثاني",
-    "التخصص الثالث",
-    "التخصص الرابع",
-    "التخصص الخامس",
-    "التخصص السادس",
-    "التخصص السابع",
-    "التخصص الثامن",
-    "التخصص التاسع",
-    "التخصص العاشر",
-  ];
 }
 
-class ChipMajorWidget extends StatelessWidget {
-  final String nameMajor;
+class CustomizeSettingsWidget extends StatelessWidget {
+  final String title;
+  final List<Widget> children;
+  const CustomizeSettingsWidget({
+    super.key,
+    required this.title,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+        child: Column(
+          textDirection: TextDirection.rtl,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              textDirection: TextDirection.rtl,
+              children: [
+                SvgPicture.asset(AppImages.generate),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Text(
+                    title,
+                    textDirection: TextDirection.rtl,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 12),
+            Wrap(
+              textDirection: TextDirection.rtl,
+              alignment: WrapAlignment.start,
+              spacing: 8,
+              runSpacing: 8,
+              children: children,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChipWidget extends StatelessWidget {
+  final String name;
   final bool? isSelected;
-  const ChipMajorWidget({super.key, this.isSelected, required this.nameMajor});
+  const ChipWidget({super.key, this.isSelected, required this.name});
 
   @override
   Widget build(BuildContext context) {
@@ -120,7 +147,7 @@ class ChipMajorWidget extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
         child: Text(
-          nameMajor,
+          name,
           style: Theme.of(context).textTheme.bodySmall?.copyWith(
             color: isSelected == true ? Colors.white : null,
             fontWeight: isSelected == true ? FontWeight.bold : null,
